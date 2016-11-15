4ba0c8e4664151bc44c2c3305e2a396f806de473
facemas22 <facemas22@DENSNET>
2016-11-15 23:56:46 +0300

    Обновлено

--- sys/themes/default_mobile/tpl/votes.tpl.php
+++ sys/themes/default_mobile/tpl/votes.tpl.php
@@ -1,27 +1,44 @@
-<div class="votes">
+<link rel="stylesheet" href="<?= $path ?>/css/progress.css" type="text/css" />
+<script charset="utf-8" src="<?= $path ?>/js/progress.min.js" type="text/javascript"></script>
+
+<div class="ui segment">
     <span class="votes_name"><?= $name ?></span>
-    <table style="width: 100%">
-        <?
-        foreach ($votes AS $vote) {
-            ?>
-            <tr>
-                <td class="vote_name" colspan="2">
-                    <?= $vote['name'] ?>
-                    <?= $vote['count'] ? ' (' . $vote['count'] . ')' : '' ?>
-                </td>
-            </tr>
-            <tr style="height: 16px;">
-                <td class="vote_container" style="width: 100%">
-                    <div class="vote_scale" style="<?= 'width: ' . max($vote['pc'], 6) . '%' ?>">
-                        <?= $vote['pc'] ?>%
+    <script type="text/javascript">
+        $('#votes')
+                .progress({
+                    active: ' {value} . {total}',
+                    success: '{total} Победа!'
+                })
+                ;
+    </script>
+    <?php
+    foreach ($votes AS $vote) {
+        ?>
+        <div class="ui indicating progress" data-value="<?= $vote['pc'] ?>" data-total="100">
+
+            <?php if ($is_add) { ?>
+                <a class="vote_plus" href="<?= $vote['url'] ?>">
+                    <div class="bar" style="<?= 'width: ' . max($vote['pc'], 6) . '%' ?>">
+                        <div class="progress"></div>
                     </div>
-                </td>
-                <? if ($is_add) { ?>
-                    <td>
-                        <a class="vote_plus" href="<?= $vote['url'] ?>">+</a>
-                    </td>
-                <? } ?>
-            </tr>        
-        <? } ?>
-    </table>
+                </a>
+            <?php } else { ?>
+                <div class="bar" style="<?= 'width: ' . max($vote['pc'], 6) . '%' ?>">
+                    <div class="progress"></div>
+                </div>
+            <?php } ?>
+            <div class="label">
+                <?= $vote['name'] ?>
+                <?= $vote['count'] ? ' (' . $vote['count'] . ')' : '' ?>
+            </div>
+        </div>
+        <script type="text/javascript">
+            $('.ui')
+                    .progress({
+                        active: ' {value} . {total}',
+                        success: '{total} Победа!'
+                    })
+                    ;
+        </script>
+    <?php } ?>
 </div>
