29010d7bd270ca74d428f28fa7fc05a25e6dc3c9
facemas22 <facemas22@DENSNET>
2016-11-15 08:13:10 +0300

    Исправлена ошибка, с обновлением листинга с AJAX

--- sys/themes/default_mobile/res/listing.js
+++ sys/themes/default_mobile/res/listing.js
@@ -32,6 +32,71 @@
     });
 
     $(function () {
+        $(".comments").each(function () {
+            var $element = $(this);
+            var id_form = $element.attr('data-form-id');
+            var url = $element.attr('data-ajax-url');
+            if (!url) {
+                return;
+            }
+            var timeout;
+
+            $(document).on('form_submit', function (event, id_form_arg) {
+                if (id_form_arg === id_form) {
+                    refresh(true);
+                }
+            });
+
+            var refresh = function (forcibly) {
+                clearTimeout(timeout);
+
+                var skip_ids = [];
+                $element.children().each(function () {
+                    skip_ids.push(this.id);
+                });
+
+                $.ajax({
+                    url: url,
+                    type: 'post',
+                    data: {skip_ids: skip_ids.join(',')},
+                    success: function (data) {
+                        var i;
+
+                        if (data.remove && data.remove.length) {
+                            for (i = 0; i < data.remove.length; i++) {
+                                $('#' + data.remove[i]).remove();
+                            }
+                        }
+
+                        if (data.add && data.add.length) {
+                            for (i = 0; i < data.add.length; i++) {
+                                var after_id = data.add[i].after_id;
+                                var $el = $(data.add[i].html).css('opacity', '0');
+
+                                if (after_id) {
+                                    $element.children('#' + after_id).after($el);
+                                } else {
+                                    $el.prependTo($element);
+                                }
+                                $el.animate({opacity: 1}, 500);
+                            }
+
+                            if (!forcibly) {
+                                $(document).trigger('newMessage');
+                            }
+                        }
+
+                        timeout = setTimeout(refresh, ajax_timeout);
+                    },
+                    error: function () {
+                        timeout = setTimeout(refresh, 60000);
+                    }
+                });
+            };
+
+            timeout = setTimeout(refresh, ajax_timeout);
+        });
+
         $(".listing").each(function () {
             var $element = $(this);
             var id_form = $element.attr('data-form-id');
