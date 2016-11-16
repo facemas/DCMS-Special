984f0085a38979a0c33ee3c733427e22f399cb6b
facemas22 <facemas22@DENSNET>
2016-11-16 22:39:25 +0300

    Обновлен внешний вид профиля

--- sys/themes/default_light/tpl/design.profile.tpl.php
+++ sys/themes/default_light/tpl/design.profile.tpl.php
@@ -1,50 +1,135 @@
-<div class="el_profile">
-    <div style="overflow: hidden;height: 180px;">
+<style>
+    ._3f2NsD-H {-webkit-align-items: stretch;-ms-flex-align: stretch;align-items: stretch;border-width: 0;border-style: solid;box-sizing: border-box;display: -webkit-flex;display: -ms-flexbox;display: flex;-webkit-flex-basis: auto;-ms-flex-preferred-size: auto;flex-basis: auto;-webkit-flex-direction: column;-ms-flex-direction: column;flex-direction: column;-webkit-flex-shrink: 0;-ms-flex-negative: 0;flex-shrink: 0;margin: 0;padding: 0;position: relative;background-color: transparent;color: inherit;font: inherit;text-align: inherit;text-decoration: none;list-style: none;min-height: 0;min-width: 0;}
+    ._1K57WeRz {-webkit-align-self: center;-ms-flex-item-align: center;align-self: center;background-postion: 50%;background-repeat: no-repeat;background-size: cover;max-width: 1500px;width: 100%;}
+    .MLZaeRvv {overflow: hidden;position: relative;width: 100%;}
+    ._2HINa6Q3 {display: block;padding-bottom: 100%;width: 100%;}
+    ._2di_LxCm {bottom: 0;height: 100%;left: 0;position: absolute;top: 0;width: 100%;}
+    ._1-I0zYji {background-color: #fff;border: 0;border-radius: .35rem;display: block;min-height: 0;-moz-force-broken-image-icon: 1;}
+    ._1eb66saq {text-align: left;}
+    ._2DggF3sL {font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Ubuntu,Helvetica Neue,sans-serif;font-size: 1rem;font-weight: 400;line-height: 1.3125;word-wrap: break-word;-webkit-font-feature-settings: "kern" 1;font-feature-settings: "kern" 1;-webkit-font-kerning: normal;font-kerning: normal;text-rendering: optimizeLegibility;}
+    ._2DggF3sL i.fa {vertical-align: baseline; box-sizing: border-box;}
+    .Fe7ul3Lt {color: inherit;display: inline;font: inherit;margin: 0;padding: 0;text-decoration: none;word-wrap: break-word;background-color: transparent;border: 0;text-align: inherit;}
+    .Z5IeoGpY {color: #657786;direction: ltr;margin-top: .1rem;unicode-bidi: embed;}
+    ._1rTfukg4[dir=ltr] {direction: ltr!important;text-align: left!important;unicode-bidi: embed;}
+    ._3fg7OHdb, ._3fg7OHdb:active, ._3fg7OHdb:focus, ._3fg7OHdb:hover {text-decoration: none!important;}
+
+    ._3WJqTbOE {font-weight: 800;}
+    ._3EJ3rVPs {font-size: .80rem;line-height: 1.3125rem;}
+    ._2ZZR2W6-,._3xujJ3kq{box-sizing:border-box;margin:0 auto;width:100%;background: #fff}
+    ._2ZZR2W6-{padding:0 .6rem}
+    ._3kOb2q-b{padding:.65625rem .6rem 0}
+    ._2CAC-K7O{box-sizing:border-box;float:left;margin:0 .3rem .65625rem 0;padding-right:.3rem;width:25%}
+    .V7IiU7kf{display:block;height:2.5rem;margin-top:2px;position:relative}
+    ._2SN3_LQA{border:2px solid #fff;border-radius:.4rem;bottom:0;left:0;margin:0 0 -2px -2px;position:absolute;width:100%}
+    ._1u_kMV_N {-webkit-align-items: center;-ms-flex-align: center;-ms-grid-row-align: center;align-items: center;-webkit-flex-direction: row;-ms-flex-direction: row;flex-direction: row;-webkit-flex-grow: 1;-ms-flex-positive: 1;flex-grow: 1;max-width: 100%;}
+    ._1pzUva68 {-webkit-justify-content: center;-ms-flex-pack: center;justify-content: center;}
+    ._3tCjwgHJ{display:-webkit-flex;display:-ms-flexbox;display:flex;float:right;margin:0 0 .65625rem}
+    ._2CFyTHU5{clear:both;display:block;margin:0 0 .65625rem}
+    .Dk8nbDvd{display:block;font-size:1.285714286rem;line-height:1.25}
+    .Z5IeoGpY{color:#657786;direction:ltr;margin-top:.1rem;unicode-bidi:embed}
+    ._2y7Q7Itc{margin-left:.6rem}
+    ._2bdbLw2N{clear:both;margin-top:.65625rem}
+    ._2bdbLw2N,.EcSZ4Fq3{margin-bottom:.65625rem}
+    .EcSZ4Fq3{display:block}
+    .CJu91lfm{margin-bottom:.65625rem}
+    .klnfEmui{color:#657786;display:inline-block;text-transform:uppercase}
+    ._27dn5W0H{margin-right:1.3125rem}
+    ._27dn5W0K{margin-left:1.3125rem}
+    .wErp7S_5{color:#14171a}
+
+    @media screen and (min-width:500px){
+        ._2bdbLw2N{width:75%}
+    }
+    @media screen and (min-width:500px){
+        ._2SN3_LQA{border-width:4px;margin:0 0 -4px -4px}
+    }
+    @media screen and (min-width:500px){
+        ._2ZZR2W6-,._3xujJ3kq{max-width:600px!important}
+    }
+
+</style>
+
+<link rel="stylesheet" href="<?= $path ?>/css/button.css" type="text/css"/>
+
+<div class="_3f2NsD-H">
+    <div class="_1K57WeRz _3f2NsD-H" style="background-image: url(<?= $fon ?>); background-color: #1d2129;">
+        <div class="MLZaeRvv">
+            <div class="_2HINa6Q3" style="padding-bottom: 33.3333%;"></div>
+            <div class="_2di_LxCm"></div>
+        </div>
+    </div>
+
+    <div class="_3kOb2q-b _3xujJ3kq">
         <?php
-        if ($fon_create[0]) {
-            echo "<a href='$fon_create[0]' class='addfon'>$fon_create[1]</a>";
+        if ($comments_avatar[0]) {
+            ?>
+            <a href='<?= $comments_avatar[0] ?>' class='_2CAC-K7O'>
+                <span class="V7IiU7kf">
+                    <!-- Выводим аватар -->
+                    <img alt="" class="_2SN3_LQA _1-I0zYji" src="<?= $avatar ?>" style="min-width: 100px;min-height: 100px;">
+                </span>
+            </a>
+            <?php
         }
         ?>
-        <!-- Выводим фон профиля -->
-        <img class="profile" style="background: #1d2129 url(<?= $fon ?>) no-repeat center; background-size: 100% 100%; -webkit-background-size: 100% 100%; padding-bottom: 56.25%;" />
-    </div>
 
-    <div style="display: inline-block;position: relative;vertical-align: bottom;text-align: center;">
-        <!-- Выводим аватар -->
-        <img class="avatar_img" src="<?= $avatar[0] ?>" style="width: 100px; height: 100px; vertical-align: top;" />
-        <!-- Выводим кнопту комментариев аватара -->
-        <span class="comments">
-            <?php
-            if ($comments_avatar[0]) {
-                echo "<a href='$comments_avatar[0]' class='comments'>$comments_avatar[1]</a>";
-            }
-            ?>
-        </span>  
-        <!-- Выводим кнопку лайков аватара -->
-        <span class="like">
-            <?php
-            if ($like_avatar[0]) {
-                echo "<a href='$like_avatar[0]' class='like'>$like_avatar[1]</a>";
-            }
-            if ($like_all_avatar[0]) {
-                echo "<a href='$like_all_avatar[0]' class='like'>$like_all_avatar[1]</a>";
-            }
-            ?>
+        <div class="_3tCjwgHJ">
+            <span class="_1u_kMV_N _1pzUva68 _3f2NsD-H">
+                <!-- Выводим действия пользователя -->
+                <?if ($gifts[0]){?><a href="<?= $gifts[0] ?>" class="ui blue basic fa button" data-tooltip='<?= __('Подарить подарок') ?>' data-position='top center'><?= $gifts[1] ?></a><?}?>
+                <?if ($mess[0]){?><a href="<?= $mess[0] ?>" class="ui blue basic fa button" data-tooltip='<?= __('Сообщение') ?>' data-position='top center'><?= $mess[1] ?></a><?}?>
+                <?if ($balls[0]){?><br /><br /><a href="<?= $balls[0] ?>" class="ui blue basic fa button" data-tooltip='<?= __('Передать баллы') ?>' data-position='top right'><?= $balls[1] ?></a><?}?>
+                <?if ($friend_add[0]){?><a href="<?= $friend_add[0] ?>" class="ui blue basic fa button" data-tooltip='<?= __('Добавить в друзья') ?>' data-position='top right'><?= $friend_add[1] ?></a><?}?>
+                <?if ($friend_del[0]){?><a href="<?= $friend_del[0] ?>" class="ui blue fa button" data-tooltip='<?= __('Удалить из друзей') ?>' data-position='top right'><?= $friend_del[1] ?></a><?}?>
+                <?if ($settings[0]){?><a href="<?= $settings[0] ?>" class="small ui grey basic fa button" data-tooltip='<?= __('Личное меню') ?>' data-position='top right'><?= $settings[1] ?></a><?}?>
+                <?if ($profile_edit[0]){?><a href="<?= $profile_edit[0] ?>" class="small ui grey basic fa button" data-tooltip='<?= __('Редактировать анкету') ?>' data-position='top right'><?= $profile_edit[1] ?></a><?}?>
+            </span>
+        </div>
+        <div class="CJu91lfm">
+            <!-- Выводим кнопку лайков аватара -->
+            <?php if ($like_avatar[0]) { ?>
+                <a class="klnfEmui _27dn5W0K _27dn5W0H _3fg7OHdb" href="<?= $like_avatar[0] ?>" data-tooltip='<?= $like_avatar[3] ?>' data-position='top center'>
+                    <span class="Fe7ul3Lt _2DggF3sL _3EJ3rVPs"><?= $like_avatar[1] ?> <?= $like_avatar[2] ?></span>
+                </a>
+            <?php } ?>
+            <!-- Выводим кнопку комментариев аватара -->
+            <?php if ($comments_avatar[0]) { ?>
+                <a class="klnfEmui _3fg7OHdb" href="<?= $comments_avatar[0] ?>" data-tooltip='<?= $comments_avatar[3] ?>' data-position='top center'>
+                    <span class="Fe7ul3Lt _2DggF3sL _3EJ3rVPs"><?= $comments_avatar[1] ?> <?= $comments_avatar[2] ?></span>
+                </a>
+            <?php } ?>
+        </div>
+        <span class="Fe7ul3Lt _2CFyTHU5 _2DggF3sL _1eb66saq">
+            <h1 class="Fe7ul3Lt Dk8nbDvd _2DggF3sL _3WJqTbOE" role="heading">
+                <?php echo "$realname $lastname"; ?>
+                <span class="<?= $online ?>"></span>
+                <span class="_2y7Q7Itc"></span>
+            </h1>
+            <span class="Fe7ul3Lt Z5IeoGpY _2DggF3sL">
+                @<?= $login ?> &#183; <?= $group_name ?>
+            </span>
+
         </span>
-    </div>
-    <div class="block">
-        <!-- Выводим логин -->
-        <h3 class="nick"><?= $login ?> <span class="<?= $online[0] ?>"><?= $online[1] ?></span></h3>
-        <!-- Выводим статус -->
-        <div class="d_r"><?= $group_name ?></div>
 
+        <?php if ($description) { ?>
+            <div class="_2bdbLw2N">
+                <span dir="ltr" class="Fe7ul3Lt EcSZ4Fq3 _1rTfukg4 _2DggF3sL">
+                    <span><?= $description ?></span>
+                </span>
+            </div>
+        <?php } ?>
+
+        <div class="CJu91lfm">
+            <?php if ($f_count[0]) { ?>
+                <a class="klnfEmui _27dn5W0H _3fg7OHdb" href="<?= $f_count[0] ?>">
+                    <span class="Fe7ul3Lt wErp7S_5 _2DggF3sL _3WJqTbOE"><?= $f_count[1] ?></span>
+                    <span class="Fe7ul3Lt _2DggF3sL _3EJ3rVPs"><?= $f_count[2] ?></span>
+                </a>
+            <?php } ?>
+            <a class="klnfEmui _3fg7OHdb" href="<?= $rating[0] ?>">
+                <span class="Fe7ul3Lt _2DggF3sL _3EJ3rVPs"><i class="fa fa-star fa-fw"></i> <?= $rating[1] ?></span>
+            </a>
+        </div>
     </div>
-    <div class="action">
-        <!-- Выводим действия пользователя -->
-        <?if ($gifts[0]){?><a href="<?= $gifts[0] ?>" class="act"><?= $gifts[1] ?></a><?}?>
-        <?if ($mess[0]){?><a href="<?= $mess[0] ?>" class="act"><?= $mess[1] ?></a><?}?>
-        <?if ($balls[0]){?><br /><br /><a href="<?= $balls[0] ?>" class="act"><?= $balls[1] ?></a><?}?>
-        <?if ($friend[0]){?><a href="<?= $friend[0] ?>" class="act"><?= $friend[1] ?></a><?}?>
-        <?if ($profile_edit[0]){?><a href="<?= $profile_edit[0] ?>" class="act"><?= $profile_edit[1] ?></a><?}?>
-    </div>
+    <div class="_3f2NsD-H"></div>
 </div>
