2eeb9d27e26b9703c6598b07a26933822d365ffe
facemas22 <facemas22@DENSNET>
2016-11-17 14:49:49 +0300

    Обновлен виджет новостей

--- sys/widgets/news/index.php
+++ sys/widgets/news/index.php
@@ -1,27 +1,36 @@
 <?php
 
 defined('SOCCMS') or die;
+$db = DB::me();
 
-$listing = new listing();
+$listing = new ui_components();
+$listing->ui_segment = true; //подключаем css segment
+$listing->class = 'segments';
+
+$res = $db->prepare("SELECT COUNT(*) FROM `news`");
+$newsCount = $res->fetchColumn();
+
 $post = $listing->post();
-$post->highlight = true;
+$post->list = true;
+$post->class = 'ui secondary segment';
 $post->icon('feed');
 $post->url = '/news/';
 $post->title = __('Все новости');
+$post->counter = $db->query(" SELECT COUNT(*) FROM `news` ")->fetchColumn();
 
-if ($dcms->widget_items_count) {
-    $db = DB::me();
-    $week = mktime(0, 0, 0, date('n'), -7);
-    $q = $db->prepare("SELECT * FROM `news` WHERE `time` > ? ORDER BY `id` DESC LIMIT " . $dcms->widget_items_count);
-    $q->execute(Array($week));
-    while ($news = $q->fetch()) {
-        $post = $listing->post();
-        $post->icon('newspaper-o');
-        $post->title = text::toValue($news['title']);
-        $post->url = '/news/comments.php?id=' . $news['id'];
-        $post->time = misc::timek($news['time']);
-        //$post->highlight = $news['time'] > NEW_TIME;
-    }
+
+$week = mktime(0, 0, 0, date('n'), -7);
+$q = $db->prepare("SELECT * FROM `news` WHERE `time` > ? ORDER BY `id` DESC LIMIT 1");
+$q->execute(Array($week));
+while ($news = $q->fetch()) {
+    $post = $listing->post();
+    $post->list = true;
+    $post->class = 'ui segment';
+    $post->icon('newspaper-o');
+    $post->title = text::toValue($news['title']);
+    $post->url = '/news/comments.php?id=' . $news['id'];
+    $post->counter = misc::timek($news['time']);
 }
+
 
 $listing->display();
