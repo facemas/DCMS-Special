17f9864ed658797106bae05d55f7938f7953c556
facemas22 <facemas22@DENSNET>
2016-11-17 22:01:55 +0300

    Обновлены ссылки

--- sys/themes/default_light/tpl/inc.user.tpl.php
+++ sys/themes/default_light/tpl/inc.user.tpl.php
@@ -1,21 +1,21 @@
 <div id="navigation_user">
-    <?
+    <?php
     if ($user->id) {
         ?>
-        <? if ($user->friend_new_count) { ?>
+        <?php if ($user->friend_new_count) { ?>
             <a id='user_friend' href='/my.friends.php'><?= __("Друзья") ?> +<span><?= $user->friend_new_count ?></span></a>
-        <? } ?>
-        <? if ($user->mail_new_count) { ?>
-            <a id='user_mail' href='/my.mail.php?only_unreaded'><?= __("Почта") ?> +<span><?= $user->mail_new_count ?></span></a>
-        <? } ?>
-        <a id='menu_user' style='font-weight: bold;' href="/menu.user.php"><?= $user->login ?></a> 
+        <?php } ?>
+        <?php if ($user->mail_new_count) { ?>
+            <a id='user_mail' href='/my.mail.php?from=new'><?= __("Почта") ?> +<span><?= $user->mail_new_count ?></span></a>
+        <?php } ?>
+        <a id='menu_user' style='font-weight: bold;' href="/profile.view.php"><?= $user->login ?></a> 
 
-        <?
+        <?php
     } else {
         ?>
         <a href="/login.php?return=<?= URL ?>"><?= __("Авторизация") ?></a>
         <a href="/reg.php?return=<?= URL ?>"><?= __("Регистрация") ?></a>
-        <?
+        <?php
     }
     ?>
 </div> 
