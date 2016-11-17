98df9a36d7f479f8b0aa14e31d59a6a03939dbcc
facemas22 <facemas22@DENSNET>
2016-11-17 11:54:23 +0300

    Добавлен элемент list

--- sys/themes/default_mobile/tpl/component.post.tpl.php
+++ sys/themes/default_mobile/tpl/component.post.tpl.php
@@ -53,12 +53,18 @@ if ($avatar) {
         if ($comments) {
             echo "<a class='author' href='$url'>$i $login $title</a>";
         }
+        if ($list) {
+            echo "<p><a class='item' href='$url'>$i $title</a></p>";
+        }
         if ($feed) {
             echo "<div class='summary'><a class='user' href='$url'>$i $login</a> $content </div>";
         }
     } else {
         if ($comments) {
             echo "<a class='author'>$i $login</a>";
+        }
+        if ($list) {
+            echo "<p><a class='item'>$i $title</a></p>";
         }
         if ($feed) {
             echo "<div class='summary'><a class='user'>$i $login</a> $content </div>";
