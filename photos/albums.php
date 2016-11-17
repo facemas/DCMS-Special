0e2acef8d6f65d26f300092403beab7ef70080a3
facemas22 <facemas22@DENSNET>
2016-11-17 22:04:04 +0300

    Обновлено

--- photos/albums.php
+++ photos/albums.php
@@ -1,4 +1,5 @@
 <?php
+
 include_once '../sys/inc/start.php';
 $doc = new document ();
 $doc->title = __('Фотоальбомы');
@@ -30,8 +31,9 @@ $photos = new files(FILES . '/.photos');
 $albums_path = FILES . '/.photos/' . $ank->id;
 
 if (!@is_dir($albums_path)) {
-    if (!$albums_dir = $photos->mkdir($ank->login, $ank->id))
-            $doc->access_denied(__('Не удалось создать папку под фотоальбомы пользователя'));
+    if (!$albums_dir = $photos->mkdir($ank->login, $ank->id)) {
+        $doc->access_denied(__('Не удалось создать папку под фотоальбомы пользователя'));
+    }
     $albums_dir->group_show = 0;
     $albums_dir->group_write = max($ank->group, 2);
     $albums_dir->group_edit = max($ank->group, 4);
@@ -48,9 +50,11 @@ if ($ank->id == $user->id && !empty($_GET ['act']) && $_GET ['act'] == 'create')
     if (!empty($_POST ['name'])) {
         $name = text::for_name($_POST ['name']);
 
-        if (!$name) $doc->err(__('Название состоит из запрещенных символов'));
-        elseif (!$album = $albums_dir->mkdir($name)) $doc->err(__('Не удалось создать альбом'));
-        else {
+        if (!$name) {
+            $doc->err(__('Название состоит из запрещенных символов'));
+        } elseif (!$album = $albums_dir->mkdir($name)) {
+            $doc->err(__('Не удалось создать альбом'));
+        } else {
             $doc->ret(__('К альбому %s', $name), 'photos.php?id=' . $ank->id . '&mp;album=' . urlencode($album->name));
             $doc->ret(__('К альбомам'), '?id=' . $ank->id);
             header('Refresh: 1; url=photos.php?id=' . $ank->id . '&album=' . urlencode($album->name));
@@ -71,10 +75,15 @@ if ($ank->id == $user->id && !empty($_GET ['act']) && $_GET ['act'] == 'create')
 $content = $albums_dir->getList('time_add:desc');
 $dirs = &$content ['dirs'];
 
-$listing = new listing();
+$listing = new ui_components();
+$listing->ui_segment = true; //подключаем css segments
+$listing->class = 'ui segments';
+
 for ($i = 0; $i < count($dirs); $i++) {
     $post = $listing->post();
-    $post->icon($dirs [$i]->icon());
+    $post->class = 'ui segment';
+    $post->list = true;
+    $post->icon($dirs[$i]->icon());
     $post->title = text::toValue($dirs [$i]->runame);
     $post->url = "photos.php?id={$ank->id}&amp;album=" . urlencode($dirs [$i]->name);
 }
@@ -83,5 +92,5 @@ $listing->display(__('Фотоальбомы отсутствуют'));
 
 
 if ($user->id == $ank->id) {
-    $doc->act(__('Создать альбом'), '?id=' . $ank->id . '&amp;act=create', false, '<i class="fa fa-plus fa-fw"></i>');
+    $doc->opt(__('Создать альбом'), '?id=' . $ank->id . '&amp;act=create', false, '<i class="fa fa-plus fa-fw"></i>');
 }
