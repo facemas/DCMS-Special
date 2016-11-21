6af7d3a087cfda279e1db67a2c4102db70992c11
facemas22 <facemas22@DENSNET>
2016-11-21 20:03:59 +0300

    Добавление подарков и категорий

---
+++ sys/preinstall/base.data.present_categories.sql
@@ -0,0 +1,7 @@
+/* Данные таблицы `present_categories` */
+
+INSERT INTO `present_categories` (`id`, `position`, `name`, `description`) VALUES
+(1, 1, 'Животные', ''),
+(2, 1, '500', ''),
+(3, 2, '200', ''),
+(4, 3, '700', '');
