3f5888bda8b32abe02f568f15a194c369be9ae68
facemas22 <facemas22@DENSNET>
2016-11-22 22:57:51 +0300

    В закрытой теме можно было писать. Исправлено

--- forum/theme.php
+++ forum/theme.php
@@ -181,107 +181,109 @@ $listing->display(__('Сообщения отсутствуют'));
 
 $pages->display('theme.php?id=' . $theme['id'] . '&amp;'); // вывод страниц
 
-$can_write = true;
-if (!$user->is_writeable) {
-    $doc->msg(__('Писать запрещено'), 'write_denied');
-    $can_write = false;
-}
+if ($theme['group_write'] <= $user->group) {
+    $can_write = true;
+    if (!$user->is_writeable) {
+        $doc->msg(__('Писать запрещено'), 'write_denied');
+        $can_write = false;
+    }
 
 # Доступ к написанию сообщений
-if ($can_write && $user->group) { #
-    if (isset($_POST['message']) && $user->group) { #
-        $message = (string) $_POST['message'];
-        $users_in_message = text::nickSearch($message);
-        $message = text::input_text($message);
+    if ($can_write && $user->group) { #
+        if (isset($_POST['message']) && $user->group) { #
+            $message = (string) $_POST['message'];
+            $users_in_message = text::nickSearch($message);
+            $message = text::input_text($message);
 
-        $af = & $_SESSION['antiflood']['forummessage'][$id_theme][$message]; // защита от дублирования сообщений в теме
+            $af = & $_SESSION['antiflood']['forummessage'][$id_theme][$message]; // защита от дублирования сообщений в теме
 
-        if ($dcms->censure && $mat = is_valid::mat($message)) {
-            $doc->err(__('Обнаружен мат: %', $mat));
-        } elseif (!empty($af) && $af > TIME - 600 || $theme['id_last'] == $user->id && $theme['time_last'] > TIME - 10) {
-            $doc->toReturn(new url('theme.php', array('id' => $theme['id'], 'page' => 'end')));
-            $doc->ret(__('В тему'), 'theme.php?id=' . $theme['id'] . '&amp;page=end');
-            $doc->err(__('Сообщение уже отправлено или вы пытаетесь ответить сами себе'));
-            exit;
-        } elseif ($dcms->forum_message_captcha && $user->group < 2 && (empty($_POST['captcha']) || empty($_POST['captcha_session']) || !captcha::check($_POST['captcha'], $_POST['captcha_session']))) {
-            $doc->err(__('Проверочное число введено неверно'));
-        } elseif ($message) {
-            $user->balls += $dcms->add_balls_message_forum;
-            $af = TIME;
-
-            $post_update = false;
-
-            $q = $db->prepare("SELECT * FROM `forum_messages` WHERE `id_theme` = ? ORDER BY `id` DESC LIMIT 1");
-            $q->execute(Array($theme['id']));
-            if ($last_post = $q->fetch()) {
-                if ($last_post['id_user'] == $user->id && $last_post['time'] > TIME - 7200) {
-                    $post_update = true;
-                    $id_message = $last_post['id'];
-                }
-            }
-
-            if ($post_update && !isset($_POST['add_file'])) {
-                $message = $last_post['message'] . "\n\n[small]Через " . misc::when(TIME - $theme['time_last'] + TIME) . ":[/small]\n" . $message;
-
-                $res = $db->prepare("UPDATE `forum_messages` SET `message` = ? WHERE `id_theme` = ? AND `id_user` = ? ORDER BY `id` DESC LIMIT 1");
-                $res->execute(Array($message, $theme['id'], $user->id));
-            } else {
-                $res = $db->prepare("INSERT INTO `forum_messages` (`id_category`, `id_topic`, `id_theme`, `id_user`, `time`, `message`, `group_show`, `group_edit`) VALUES (?,?,?,?,?,?,?,?)");
-                $res->execute(Array($theme['id_category'], $theme['id_topic'], $theme['id'], $user->id, TIME, $message, $theme['group_show'], $theme['group_edit']));
-
-                $id_message = $db->lastInsertId();
-            }
-            if (isset($_POST['add_file'])) {
-                $doc->toReturn(new url('message.files.php', array('id' => $id_message,
-                    'return' => new url('theme.php', array(
-                        'id' => $theme['id'],
-                        'page' => 'end')
-                ))));
-                $doc->opt(__('Добавить файлы'), 'message.files.php?id=' . $id_message . '&amp;return=' . urlencode('theme.php?id=' . $theme['id'] . '&page=end'));
-            } else {
-                $doc->toReturn(new url('theme.php', array(
-                    'id' => $theme['id'],
-                    'page' => 'end'
-                )));
+            if ($dcms->censure && $mat = is_valid::mat($message)) {
+                $doc->err(__('Обнаружен мат: %', $mat));
+            } elseif (!empty($af) && $af > TIME - 600 || $theme['id_last'] == $user->id && $theme['time_last'] > TIME - 10) {
+                $doc->toReturn(new url('theme.php', array('id' => $theme['id'], 'page' => 'end')));
                 $doc->ret(__('В тему'), 'theme.php?id=' . $theme['id'] . '&amp;page=end');
-            }
+                $doc->err(__('Сообщение уже отправлено или вы пытаетесь ответить сами себе'));
+                exit;
+            } elseif ($dcms->forum_message_captcha && $user->group < 2 && (empty($_POST['captcha']) || empty($_POST['captcha_session']) || !captcha::check($_POST['captcha'], $_POST['captcha_session']))) {
+                $doc->err(__('Проверочное число введено неверно'));
+            } elseif ($message) {
+                $user->balls += $dcms->add_balls_message_forum;
+                $af = TIME;
 
-            if ($users_in_message) {
-                for ($i = 0; $i < count($users_in_message) && $i < 20; $i++) {
-                    $user_id_in_message = $users_in_message[$i];
-                    if ($user_id_in_message == $user->id) {
-                        continue;
-                    }
-                    $ank_in_message = new user($user_id_in_message);
-                    if ($ank_in_message->notice_mention) {
-                        $res = $db->prepare("SELECT COUNT(*) FROM `forum_messages` WHERE `id_theme` = ? AND `group_show` <= ?");
-                        $res->execute(Array($theme['id'], $ank_in_message->group));
-                        $count_posts_for_user = $res->fetchColumn();
-                        $ank_in_message->mess("[user]{$user->id}[/user] упомянул" . ($user->sex ? '' : 'а') . " о Вас на форуме в [url=/forum/message.php?id_message={$id_message}]сообщении[/url] в теме [url=/forum/theme.php?id={$theme['id']}&postnum={$count_posts_for_user}#message{$id_message}]{$theme['name']}[/url]");
+                $post_update = false;
+
+                $q = $db->prepare("SELECT * FROM `forum_messages` WHERE `id_theme` = ? ORDER BY `id` DESC LIMIT 1");
+                $q->execute(Array($theme['id']));
+                if ($last_post = $q->fetch()) {
+                    if ($last_post['id_user'] == $user->id && $last_post['time'] > TIME - 7200) {
+                        $post_update = true;
+                        $id_message = $last_post['id'];
                     }
                 }
+
+                if ($post_update && !isset($_POST['add_file'])) {
+                    $message = $last_post['message'] . "\n\n[small]Через " . misc::when(TIME - $theme['time_last'] + TIME) . ":[/small]\n" . $message;
+
+                    $res = $db->prepare("UPDATE `forum_messages` SET `message` = ? WHERE `id_theme` = ? AND `id_user` = ? ORDER BY `id` DESC LIMIT 1");
+                    $res->execute(Array($message, $theme['id'], $user->id));
+                } else {
+                    $res = $db->prepare("INSERT INTO `forum_messages` (`id_category`, `id_topic`, `id_theme`, `id_user`, `time`, `message`, `group_show`, `group_edit`) VALUES (?,?,?,?,?,?,?,?)");
+                    $res->execute(Array($theme['id_category'], $theme['id_topic'], $theme['id'], $user->id, TIME, $message, $theme['group_show'], $theme['group_edit']));
+
+                    $id_message = $db->lastInsertId();
+                }
+                if (isset($_POST['add_file'])) {
+                    $doc->toReturn(new url('message.files.php', array('id' => $id_message,
+                        'return' => new url('theme.php', array(
+                            'id' => $theme['id'],
+                            'page' => 'end')
+                    ))));
+                    $doc->opt(__('Добавить файлы'), 'message.files.php?id=' . $id_message . '&amp;return=' . urlencode('theme.php?id=' . $theme['id'] . '&page=end'));
+                } else {
+                    $doc->toReturn(new url('theme.php', array(
+                        'id' => $theme['id'],
+                        'page' => 'end'
+                    )));
+                    $doc->ret(__('В тему'), 'theme.php?id=' . $theme['id'] . '&amp;page=end');
+                }
+
+                if ($users_in_message) {
+                    for ($i = 0; $i < count($users_in_message) && $i < 20; $i++) {
+                        $user_id_in_message = $users_in_message[$i];
+                        if ($user_id_in_message == $user->id) {
+                            continue;
+                        }
+                        $ank_in_message = new user($user_id_in_message);
+                        if ($ank_in_message->notice_mention) {
+                            $res = $db->prepare("SELECT COUNT(*) FROM `forum_messages` WHERE `id_theme` = ? AND `group_show` <= ?");
+                            $res->execute(Array($theme['id'], $ank_in_message->group));
+                            $count_posts_for_user = $res->fetchColumn();
+                            $ank_in_message->mess("[user]{$user->id}[/user] упомянул" . ($user->sex ? '' : 'а') . " о Вас на форуме в [url=/forum/message.php?id_message={$id_message}]сообщении[/url] в теме [url=/forum/theme.php?id={$theme['id']}&postnum={$count_posts_for_user}#message{$id_message}]{$theme['name']}[/url]");
+                        }
+                    }
+                }
+
+                $doc->msg(__('Сообщение успешно отправлено'));
+                $res = $db->prepare("UPDATE `forum_themes` SET `time_last` = ?, `id_last` = ? WHERE `id` = ? LIMIT 1");
+                $res->execute(Array(TIME, $user->id, $theme['id']));
+                exit;
+            } else {
+                $doc->err(__('Сообщение пусто'));
             }
-
-            $doc->msg(__('Сообщение успешно отправлено'));
-            $res = $db->prepare("UPDATE `forum_themes` SET `time_last` = ?, `id_last` = ? WHERE `id` = ? LIMIT 1");
-            $res->execute(Array(TIME, $user->id, $theme['id']));
-            exit;
-        } else {
-            $doc->err(__('Сообщение пусто'));
         }
+
+        $form = new form(new url());
+        $form->textarea('message', __('Сообщение'));
+        $form->checkbox('add_file', __('Добавить файл'));
+        $form->block('<br />');
+
+        if ($dcms->forum_message_captcha && $user->group < 2) {
+            $form->captcha();
+        }
+
+        $form->button(__('Отправить'));
+        $form->display();
     }
-
-    $form = new form(new url());
-    $form->textarea('message', __('Сообщение'));
-    $form->checkbox('add_file', __('Добавить файл'));
-    $form->block('<br />');
-
-    if ($dcms->forum_message_captcha && $user->group < 2) {
-        $form->captcha();
-    }
-
-    $form->button(__('Отправить'));
-    $form->display();
 }
 
 if ($theme['group_write'] <= $user->group) {
@@ -292,6 +294,6 @@ if ($user->group >= 2 || $theme['group_edit'] <= $user->group || ($user->id && $
     $doc->opt(__('Действия'), 'theme.actions.php?id=' . $theme['id'], false, '<i class="fa fa-location-arrow fa-fw"></i>');
 }
 
-$doc->act($theme['topic_name'], 'topic.php?id=' . $theme['id_topic']);
-$doc->act($theme['category_name'], 'category.php?id=' . $theme['id_category']);
+$doc->ret($theme['topic_name'], 'topic.php?id=' . $theme['id_topic']);
+$doc->ret($theme['category_name'], 'category.php?id=' . $theme['id_category']);
 $doc->ret(__('Форум'), './');
